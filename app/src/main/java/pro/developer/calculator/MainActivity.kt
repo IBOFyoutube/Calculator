2f9ee0def29b600c5320792e9e669c8e14376d32
IBOFyoutube <isdevacademy2005@gmail.com>
2023-09-20 16:53:02 +0500

    Calculator

--- app/src/main/java/pro/developer/calculator/MainActivity.kt
+++ app/src/main/java/pro/developer/calculator/MainActivity.kt
@@ -14,8 +14,38 @@ import javax.xml.xpath.XPathExpression
 
 class MainActivity : AppCompatActivity() {
 
-    private var canAddOperation = false
-    private var canAddDecimal = true
+    lateinit var tvsec: TextView
+    lateinit var tvMain: TextView
+    lateinit var bac: Button
+    lateinit var bc: Button
+    lateinit var bbrac1: Button
+    lateinit var bbrac2: Button
+    lateinit var bsin: Button
+    lateinit var bcos: Button
+    lateinit var btan: Button
+    lateinit var blog: Button
+    lateinit var bln: Button
+    lateinit var bfact: Button
+    lateinit var bsquare: Button
+    lateinit var bsqrt: Button
+    lateinit var binv: Button
+    lateinit var b0: Button
+    lateinit var b9: Button
+    lateinit var b8: Button
+    lateinit var b7: Button
+    lateinit var b6: Button
+    lateinit var b5: Button
+    lateinit var b4: Button
+    lateinit var b3: Button
+    lateinit var b2: Button
+    lateinit var b1: Button
+    lateinit var bpi: Button
+    lateinit var bmul: Button
+    lateinit var bminus: Button
+    lateinit var bplus: Button
+    lateinit var bequal: Button
+    lateinit var bdot: Button
+    lateinit var bdiv: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,164 +53,300 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
 
-        // chaqiruv
-        val txt_result = findViewById<TextView>(R.id.txt_result)
-        val working_result = findViewById<TextView>(R.id.working_result)
-        // Numbers
-        var btn_0 = findViewById<Button>(R.id.btn_0)
-        var btn_1 = findViewById<Button>(R.id.btn_1)
-        var btn_2 = findViewById<Button>(R.id.btn_2)
-        var btn_3 = findViewById<Button>(R.id.btn_3)
-        var btn_4 = findViewById<Button>(R.id.btn_4)
-        var btn_5 = findViewById<Button>(R.id.btn_5)
-        var btn_6 = findViewById<Button>(R.id.btn_6)
-        var btn_7 = findViewById<Button>(R.id.btn_7)
-        var btn_8 = findViewById<Button>(R.id.btn_8)
-        var btn_9 = findViewById<Button>(R.id.btn_9)
-        // Buttons first
-        var btn_clear = findViewById<Button>(R.id.btn_clear)
-        var btn_qavs1 = findViewById<Button>(R.id.btn_qavs1)
-        var btn_qavs2 = findViewById<Button>(R.id.btn_qavs2)
-        var btn_divide = findViewById<Button>(R.id.btn_divide)
-        // Buttons second
-        var btn_multiply = findViewById<Button>(R.id.btn_multiply)
-        var btn_minus = findViewById<Button>(R.id.btn_minus)
-        var btn_plus = findViewById<Button>(R.id.btn_plus)
-        // Buttons third
-        var btn_dot = findViewById<Button>(R.id.btn_dot)
-        var btn_back = findViewById<Button>(R.id.btn_back)
-        var btn_equals = findViewById<Button>(R.id.btn_equals)
+        // button larni yuklash
+        tvsec = findViewById(R.id.idTVSecondary)
+        tvMain = findViewById(R.id.idTVprimary)
+        bac = findViewById(R.id.bac)
+        bc = findViewById(R.id.bc)
+        bbrac1 = findViewById(R.id.bbrac1)
+        bbrac2 = findViewById(R.id.bbrac2)
+        bsin = findViewById(R.id.bsin)
+        bcos = findViewById(R.id.bcos)
+        btan = findViewById(R.id.btan)
+        blog = findViewById(R.id.blog)
+        bln = findViewById(R.id.bln)
+        bfact = findViewById(R.id.bfact)
+        bsquare = findViewById(R.id.bsquare)
+        bsqrt = findViewById(R.id.bsqrt)
+        binv = findViewById(R.id.binv)
+        b0 = findViewById(R.id.b0)
+        b9 = findViewById(R.id.b9)
+        b8 = findViewById(R.id.b8)
+        b7 = findViewById(R.id.b7)
+        b6 = findViewById(R.id.b6)
+        b5 = findViewById(R.id.b5)
+        b4 = findViewById(R.id.b4)
+        b3 = findViewById(R.id.b3)
+        b2 = findViewById(R.id.b2)
+        b1 = findViewById(R.id.b1)
+        bpi = findViewById(R.id.bpi)
+        bmul = findViewById(R.id.bmul)
+        bminus = findViewById(R.id.bminus)
+        bplus = findViewById(R.id.bplus)
+        bequal = findViewById(R.id.bequal)
+        bdot = findViewById(R.id.bdot)
+        bdiv = findViewById(R.id.bdiv)
 
-
-
-
-        // clear result
-        btn_clear.setOnClickListener {
-            working_result.setText("")
-            txt_result.setText("")
-            true
+        // button larni ishga tushurish
+        b1.setOnClickListener {
+            // on below line we are appending
+            // the expression to our text view.
+            tvMain.text = (tvMain.text.toString() + "1")
+        }
+        b2.setOnClickListener {
+            // on below line we are appending
+            // the expression to our text view.
+            tvMain.text = (tvMain.text.toString() + "2")
+        }
+        b3.setOnClickListener {
+            // on below line we are appending
+            // the expression to our text view.
+            tvMain.text = (tvMain.text.toString() + "3")
+        }
+        b4.setOnClickListener {
+            tvMain.text = (tvMain.text.toString() + "4")
+        }
+        b5.setOnClickListener {
+            tvMain.text = (tvMain.text.toString() + "5")
+        }
+        b6.setOnClickListener {
+            tvMain.text = (tvMain.text.toString() + "6")
+        }
+        b7.setOnClickListener {
+            tvMain.text = (tvMain.text.toString() + "7")
+        }
+        b8.setOnClickListener {
+            tvMain.text = (tvMain.text.toString() + "8")
+        }
+        b9.setOnClickListener {
+            tvMain.text = (tvMain.text.toString() + "9")
+        }
+        b0.setOnClickListener {
+            tvMain.text = (tvMain.text.toString() + "0")
+        }
+        bdot.setOnClickListener {
+            tvMain.text = (tvMain.text.toString() + ".")
+        }
+        bplus.setOnClickListener {
+            tvMain.text = (tvMain.text.toString() + "+")
+        }
+        bdiv.setOnClickListener {
+            tvMain.text = (tvMain.text.toString() + "/")
+        }
+        bbrac1.setOnClickListener {
+            tvMain.text = (tvMain.text.toString() + "(")
+        }
+        bbrac2.setOnClickListener {
+            tvMain.text = (tvMain.text.toString() + ")")
+        }
+        bpi.setOnClickListener {
+            // on clicking on pi button we are adding
+            // pi value as 3.142 to our current value.
+            tvMain.text = (tvMain.text.toString() + "3.142")
+            tvsec.text = (bpi.text.toString())
+        }
+        bsin.setOnClickListener {
+            tvMain.text = (tvMain.text.toString() + "sin")
+        }
+        bcos.setOnClickListener {
+            tvMain.text = (tvMain.text.toString() + "cos")
+        }
+        btan.setOnClickListener {
+            tvMain.text = (tvMain.text.toString() + "tan")
+        }
+        binv.setOnClickListener {
+            tvMain.text = (tvMain.text.toString() + "^" + "(-1)")
+        }
+        bln.setOnClickListener {
+            tvMain.text = (tvMain.text.toString() + "ln")
+        }
+        blog.setOnClickListener {
+            tvMain.text = (tvMain.text.toString() + "log")
         }
 
-        // symbols
-        btn_qavs1.setOnClickListener {
-            working_result.text=addInputText("(")
+        bminus.setOnClickListener {
+            val str: String = tvMain.text.toString()
+            if (!str.get(index = str.length - 1).equals("-")) {
+                tvMain.text = (tvMain.text.toString() + "-")
+            }
         }
-        btn_qavs2.setOnClickListener {
-            working_result.text=addInputText(")")
+        bmul.setOnClickListener {
+            val str: String = tvMain.text.toString()
+            if (!str.get(index = str.length - 1).equals("*")) {
+                tvMain.text = (tvMain.text.toString() + "*")
+            }
         }
-        btn_dot.setOnClickListener {
-            working_result.text=addInputText(".")
-        }
-        btn_plus.setOnClickListener {
-            working_result.text=addInputText("+")
-        }
-        btn_minus.setOnClickListener {
-            working_result.text=addInputText("-")
-        }
-        btn_divide.setOnClickListener {
-            working_result.text=addInputText("÷")
-        }
-        btn_multiply.setOnClickListener {
-            working_result.text=addInputText("x")
-        }
+        bsqrt.setOnClickListener {
+            if (tvMain.text.toString().isEmpty()) {
+                // Error message chiqadi
+                Toast.makeText(this, "Please enter a valid number..", Toast.LENGTH_SHORT).show()
+            } else {
+                val str: String = tvMain.text.toString()
+                val r = Math.sqrt(str.toDouble())
 
-        // numbers
-        btn_0.setOnClickListener {
-            working_result.text=addInputText("0")
+                val result = r.toString()
+                tvMain.setText(result)
+            }
         }
-        btn_1.setOnClickListener {
-            working_result.text=addInputText("1")
+        bequal.setOnClickListener {
+            val str: String = tvMain.text.toString()
+            val result: Double = evaluate(str)
+            val r = result.toString()
+            tvMain.setText(r)
+            tvsec.text = str
         }
-        btn_2.setOnClickListener {
-            working_result.text=addInputText("2")
+        bac.setOnClickListener {
+            tvMain.setText("")
+            tvsec.setText("")
         }
-        btn_3.setOnClickListener {
-            working_result.text=addInputText("3")
+        bc.setOnClickListener {
+            var str: String = tvMain.text.toString()
+            if (!str.equals("")) {
+                str = str.substring(0, str.length - 1)
+                tvMain.text = str
+            }
         }
-        btn_4.setOnClickListener {
-            working_result.text=addInputText("4")
+        bsquare.setOnClickListener {
+            if (tvMain.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please enter a valid number..", Toast.LENGTH_SHORT).show()
+            } else {
+
+                val d: Double = tvMain.getText().toString().toDouble()
+                val square = d * d
+
+                tvMain.setText(square.toString())
+
+                tvsec.text = "$d²"
+            }
         }
-        btn_5.setOnClickListener {
-            working_result.text=addInputText("5")
-        }
-        btn_6.setOnClickListener {
-            working_result.text=addInputText("6")
-        }
-        btn_7.setOnClickListener {
-            working_result.text=addInputText("7")
-        }
-        btn_8.setOnClickListener {
-            working_result.text=addInputText("8")
-        }
-        btn_9.setOnClickListener {
-            working_result.text=addInputText("9")
-        }
-        /// press equals result
-        btn_equals.setOnClickListener {
-            showResult()
-        }
+        bfact.setOnClickListener {
+            if (tvMain.text.toString().isEmpty()) {
 
-        //press back button
-        btn_back.setOnClickListener {
-         backSpaceAction()
-        }
-    }
+                Toast.makeText(this, "Please enter a valid number..", Toast.LENGTH_SHORT).show()
+            } else {
 
-    // functions
-    private fun addInputText(buttonValue:String):String{
-
-        val working_result = findViewById<TextView>(R.id.working_result)
-
-        return "${working_result.text} $buttonValue"
-
-    }
-
-
-    private fun getInputExpression():String{
-        val working_result = findViewById<TextView>(R.id.working_result)
-
-        var expression=working_result.text.replace(Regex("÷"),"/")
-        expression=expression.replace(Regex("x"),"*")
-        return expression
-    }
-
-    private fun showResult() {
-
-        val txt_result = findViewById<TextView>(R.id.txt_result)
-        val working_result = findViewById<TextView>(R.id.working_result)
-
-        try {
-
-            val expression=getInputExpression()
-            val result = Expression(expression).calculate()
-
-            if (result.isNaN()){
-                // Show Error Message
-                txt_result.text="Error"
-                txt_result.setTextColor(ContextCompat.getColor(this,R.color.red))
-
-            }else{
-                // Show Result
-                txt_result.text=DecimalFormat("0.######").format(result).toString()
-                txt_result.setTextColor(ContextCompat.getColor(this,R.color.green))
-
-
-
+                val value: Int = tvMain.text.toString().toInt()
+                val fact: Int = factorial(value)
+                tvMain.setText(fact.toString())
+                tvsec.text = "$value`!"
             }
 
-        }catch (e:Exception){
-            // Show Error Message
-            txt_result.text="Error"
-            txt_result.setTextColor(ContextCompat.getColor(this,R.color.red))
-
         }
+
     }
 
-    private fun backSpaceAction(){
-        val working_result = findViewById<TextView>(R.id.working_result)
-        val length= working_result.length()
-        if (length>0){
-            working_result.text=working_result.text.subSequence(0,length-1)
-        }
+    fun factorial(n: Int): Int {
+
+        return if (n == 1 || n == 0) 1 else n * factorial(n - 1)
+    }
+
+    fun evaluate(str: String): Double {
+        return object : Any() {
+
+            var pos = -1
+            var ch = 0
+
+
+            fun nextChar() {
+
+                ch = if (++pos < str.length) str[pos].toInt() else -1
+            }
+
+
+            fun eat(charToEat: Int): Boolean {
+                while (ch == ' '.toInt()) nextChar()
+
+                if (ch == charToEat) {
+                    nextChar()
+                    return true
+                }
+                return false
+            }
+
+
+
+            fun parse(): Double {
+                nextChar()
+                val x = parseExpression()
+                if (pos < str.length) throw RuntimeException("Unexpected: " + ch.toChar())
+                return x
+            }
+
+
+
+            fun parseExpression(): Double {
+                var x = parseTerm()
+                while (true) {
+                    if (eat('+'.toInt())) x += parseTerm() // qoshish
+                    else if (eat('-'.toInt())) x -= parseTerm() // minus
+                    else return x
+                }
+            }
+
+
+            fun parseTerm(): Double {
+                var x = parseFactor()
+                while (true) {
+                    if (eat('*'.toInt())) x *= parseFactor() // kopaytirish
+                    else if (eat('/'.toInt())) x /= parseFactor() // bolish
+                    else return x
+                }
+            }
+
+
+            fun parseFactor(): Double {
+
+                if (eat('+'.toInt())) return parseFactor() // plus
+                if (eat('-'.toInt())) return -parseFactor() //  minus
+
+                var x: Double
+
+                val startPos = pos
+
+                if (eat('('.toInt())) {
+                    x = parseExpression()
+                    eat(')'.toInt())
+                } else if (ch >= '0'.toInt() && ch <= '9'.toInt() || ch == '.'.toInt()) {
+
+                    while (ch >= '0'.toInt() && ch <= '9'.toInt() || ch == '.'.toInt()) nextChar()
+
+                    x = str.substring(startPos, pos).toDouble()
+                } else if (ch >= 'a'.toInt() && ch <= 'z'.toInt()) {
+
+                    while (ch >= 'a'.toInt() && ch <= 'z'.toInt()) nextChar()
+                    val func = str.substring(startPos, pos)
+
+                    x = parseFactor()
+
+                    x =
+                        if (func == "sqrt") Math.sqrt(x)
+
+                        else if (func == "sin") Math.sin(
+                            Math.toRadians(x)
+
+                        ) else if (func == "cos") Math.cos(
+                            Math.toRadians(x)
+
+                        ) else if (func == "tan")
+                            Math.tan(Math.toRadians(x))
+
+                        else if (func == "log")
+                            Math.log10(x)
+
+                        else if (func == "ln") Math.log(x)
+
+                        else throw RuntimeException(
+                            "Unknown function: $func"
+                        )
+                } else {
+
+                    throw RuntimeException("Unexpected: " + ch.toChar())
+                }
+
+                if (eat('^'.toInt())) x = Math.pow(x, parseFactor())
+                return x
+            }
+
+        }.parse()
     }
 }
